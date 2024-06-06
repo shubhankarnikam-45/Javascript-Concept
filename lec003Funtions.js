50cd23c2afbfb3aecc120c39d1ea835a4048953d
shubhankarnikam-45 <shubhankarnikam45@gmail.com>
2024-06-06 21:02:44 +0530

    function code is added

---
+++ lec003Funtions.js
@@ -0,0 +1,28 @@
+
+//syntax.
+// function demo()
+// {
+//     console.log("vipul");
+// }
+// demo();
+
+//in js 3 types of functions.
+//1. normal functions.
+// function demo()
+// {
+//     console.log("normal function");
+// }
+// demo();
+
+//function expression.
+// let temp = function demo1()
+// {
+//     console.log("function expression");
+// }
+// temp();
+
+//arrow function.
+// let arrowFun = ()=>{
+//     console.log("arrow function");
+// }
+// arrowFun();
