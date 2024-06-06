8cd9fef2c175e5cb6ec2b5f34351db70ad122109
shubhankarnikam-45 <shubhankarnikam45@gmail.com>
2024-06-06 20:55:05 +0530

    code of array concept

---
+++ lec002Array.js
@@ -0,0 +1,20 @@
+//why is array ?
+
+//history of array. (why)
+// let arr = ["vipul" , "kiran", "tukaram", "ram"];
+// console.log(arr);
+
+//create of array.
+// let arr = []; syatax.
+
+let arr = ["shubhankar",1];
+
+//to add element in array.
+arr.push(20);
+
+//to remove element in array.
+arr.pop();
+console.log(arr);
+
+//in javascript array is object.
+console.log(typeof arr);
